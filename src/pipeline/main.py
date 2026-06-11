0022fb389e4db01db98127a8be728ce14206d833
Safaelbenna <go69yop@mytum.de>
2026-06-11 20:35:28 +0200

    Evaluation of runtime of the whole pipeline

--- src/pipeline/main.py
+++ src/pipeline/main.py
@@ -2,6 +2,7 @@ from load_to_virtuoso import load_all_ttl_files
 from test_querying import run_query, graph_exists
 from config import GRAPH_v1, GRAPH_v2, GRAPH_2021, GRAPH_2022
 from delta_computation import additions_computation, deletions_computation, convert_isql_output_to_nt
+import time
 
 
 def preview_graph(graph_uri: str) -> None:
@@ -21,6 +22,7 @@ def preview_graph(graph_uri: str) -> None:
     print("-" * 50)
     print(result)
 
+start = time.perf_counter()
 print("Loading TTL files...")
 
 load_all_ttl_files()
@@ -45,12 +47,20 @@ preview_graph(GRAPH_2021)
 preview_graph(GRAPH_2022)
 
 
-print("\nDelta Computation")
-additions_computation(GRAPH_v1, GRAPH_v2)
-deletions_computation(GRAPH_v1, GRAPH_v2)
-additions_computation(GRAPH_2021, GRAPH_2022)
-deletions_computation(GRAPH_2021, GRAPH_2022)
+#print("\nDelta Computation")
+#additions_computation(GRAPH_v1, GRAPH_v2)
+#deletions_computation(GRAPH_v1, GRAPH_v2)
+#additions_computation(GRAPH_2021, GRAPH_2022)
+#deletions_computation(GRAPH_2021, GRAPH_2022)
 #convert_isql_output_to_nt("additions_2022_minus_2021.txt", "additions_2022_minus_2021.ttl")
+
+end = time.perf_counter()
+seconds = end - start
+
+print(f"Total runtime of Pipeline.\n")
+print(f"Time: {seconds:.2f} seconds")
+print(f"Time: {seconds/60:.2f} minutes")
+
 
 
 
