d72342522f07561cb7d464b5a7c7ff171362cb1d
onsbe <ons.benmakhlouf1@gmail.com>
2026-06-26 11:41:32 +0200

    fixed time func

--- src/pipeline/delta_computation.py
+++ src/pipeline/delta_computation.py
@@ -1,120 +1,6 @@
 import subprocess
 import time
 
-#delta computation with storing into files
-# def additions_computation(graph_uri1: str, graph_uri2: str) -> None:
-#     query = f"""
-#     SPARQL
-#     SELECT ?s ?p ?o
-#     WHERE {{
-#     GRAPH <{graph_uri2}> {{
-#         ?s ?p ?o .
-#     }}
-
-#     FILTER NOT EXISTS {{
-#         GRAPH <{graph_uri1}> {{
-#         ?s ?p ?o .
-#         }}
-#     }}
-#     }};
-#     exit;
-#     """
-
-#     start = time.time()
-#     value = "v" if ("v" in graph_uri1) else "202"
-
-#     print("\nComputing additions...")
-
-#     with open(f"additions_{value}2_minus_{value}1.txt", "w") as output:
-#         subprocess.run(
-#             ["docker", "exec", "-i", "deltagraph_virtuoso", "isql", "1111", "dba", "mysecret"],
-#             input=query,
-#             stdout=output,
-#             text=True
-#         )
-
-#     end = time.time()
-
-#     with open(f"additions_{value}2_minus_{value}1.txt", "r", errors="replace") as file:
-#         for line in file:
-#             if "Rows. --" in line:
-#                 parts = line.split()
-#                 rows, msec = int(parts[0]), int(parts[3])
-#                 print(f"{rows:,} rows in ({msec})ms {msec/1000:.1f}s ({msec/60000:.1f} min)")
-
-
-#     seconds = end - start
-
-#     print("Finished")
-#     print(f"Time: {seconds:.2f} seconds")
-#     print(f"Time: {seconds/60:.2f} minutes")
-
-#     print("converting response in a TTL file...\n")
-#     start = time.time()
-#     convert_isql_output_to_nt(f"additions_{value}2_minus_{value}1.txt", f"additions_{value}2_minus_{value}1.ttl")
-#     end = time.time()
-#     seconds = end - start
-
-#     print("Finished")
-#     print(f"Time: {seconds:.2f} seconds")
-#     print(f"Time: {seconds/60:.2f} minutes")
-
-# def deletions_computation(graph_uri1: str, graph_uri2: str) -> None:
-#     query = f"""
-#     SPARQL
-#     SELECT ?s ?p ?o
-#     WHERE {{
-#     GRAPH <{graph_uri1}> {{
-#         ?s ?p ?o .
-#     }}
-
-#     FILTER NOT EXISTS {{
-#         GRAPH <{graph_uri2}> {{
-#         ?s ?p ?o .
-#         }}
-#     }}
-#     }};
-#     exit;
-#     """
-
-#     start = time.time()
-#     value = "v" if ("v" in graph_uri1) else "202"
-
-#     print("\nComputing deletions...")
-
-#     with open(f"deletions_{value}1_minus_{value}2.txt", "w") as output:
-#         subprocess.run(
-#             ["docker", "exec", "-i", "deltagraph_virtuoso", "isql", "1111", "dba", "mysecret"],
-#             input=query,
-#             stdout=output,
-#             text=True
-#         )
-
-#     end = time.time()
-
-#     with open(f"deletions_{value}1_minus_{value}2.txt", "r", errors="replace") as file:
-#         for line in file:
-#             if "Rows. --" in line:
-#                 parts = line.split()
-#                 rows, msec = int(parts[0]), int(parts[3])
-#                 print(f"{rows:,} rows in ({msec})ms {msec/1000:.1f}s ({msec/60000:.1f} min)")
-
-#     seconds = end - start
-
-#     print("Finished")
-#     print(f"Time: {seconds:.2f} seconds")
-#     print(f"Time: {seconds/60:.2f} minutes")
-
-#     print("converting response in a TTL file...\n")
-#     start = time.time()
-#     convert_isql_output_to_nt(f"deletions_{value}1_minus_{value}2.txt", f"deletions_{value}1_minus_{value}2.ttl")
-#     end = time.time()
-#     seconds = end - start
-
-#     print("Finished")
-#     print(f"Time: {seconds:.2f} seconds")
-#     print(f"Time: {seconds/60:.2f} minutes")
-
 
 def additions_computation(graph_uri1: str, graph_uri2: str) -> None:
     query = f"""
@@ -134,7 +20,7 @@ def additions_computation(graph_uri1: str, graph_uri2: str) -> None:
     exit;
     """
 
-    start = time.time()
+    start = time.perf_counter()
     value = "v" if ("v" in graph_uri1) else "202"
     dataset_name = graph_uri1.rstrip("/").split("/")[-1]
 
@@ -148,7 +34,7 @@ def additions_computation(graph_uri1: str, graph_uri2: str) -> None:
             text=True
         )
 
-    end = time.time()
+    end = time.perf_counter()
 
     with open(f"additions_{dataset_name}_{value}2_minus_{value}1.txt", "r", errors="replace") as file:
         for line in file:
@@ -164,12 +50,12 @@ def additions_computation(graph_uri1: str, graph_uri2: str) -> None:
     print(f"Time: {seconds/60:.2f} minutes")
 
     print("converting response in a TTL file...\n")
-    start = time.time()
+    start = time.perf_counter()
     convert_isql_output_to_nt(
         f"additions_{dataset_name}_{value}2_minus_{value}1.txt",
         f"test data/additions_{dataset_name}_{value}2_minus_{value}1.ttl"
     )
-    end = time.time()
+    end = time.perf_counter()
     seconds = end - start
 
     print("Finished")
@@ -195,7 +81,7 @@ def deletions_computation(graph_uri1: str, graph_uri2: str) -> None:
     exit;
     """
 
-    start = time.time()
+    start = time.perf_counter()
     value = "v" if ("v" in graph_uri1) else "202"
     dataset_name = graph_uri1.rstrip("/").split("/")[-1]
 
@@ -209,7 +95,8 @@ def deletions_computation(graph_uri1: str, graph_uri2: str) -> None:
             text=True
         )
 
-    end = time.time()
+    end = time.perf_counter()
+
 
     with open(f"deletions_{dataset_name}_{value}1_minus_{value}2.txt", "r", errors="replace") as file:
         for line in file:
@@ -225,12 +112,13 @@ def deletions_computation(graph_uri1: str, graph_uri2: str) -> None:
     print(f"Time: {seconds/60:.2f} minutes")
 
     print("converting response in a TTL file...\n")
-    start = time.time()
+    start = time.perf_counter()
     convert_isql_output_to_nt(
         f"deletions_{dataset_name}_{value}1_minus_{value}2.txt",
         f"test data/deletions_{dataset_name}_{value}1_minus_{value}2.ttl"
     )
-    end = time.time()
+    end = time.perf_counter()
+
     seconds = end - start
 
     print("Finished")
@@ -253,7 +141,7 @@ def to_nt_object(value: str) -> str:
 def convert_isql_output_to_nt(input_path: str, output_path: str) -> None:
     converted = 0
     skipped = 0
-    start = time.time()
+    start = time.perf_counter()
     with open(input_path, "r", encoding="utf-8", errors="replace") as inp, \
          open(output_path, "w", encoding="utf-8") as out:
 
@@ -282,7 +170,8 @@ def convert_isql_output_to_nt(input_path: str, output_path: str) -> None:
 
             out.write(f"<{s}> <{p}> {to_nt_object(o)} .\n")
             converted += 1
-    end = time.time()
+    end = time.perf_counter()
+
 
     print(f"Converted triples: {converted}")
     print(f"Skipped lines: {skipped}")
